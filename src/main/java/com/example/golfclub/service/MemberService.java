77fda4ab4271a7a5f992dcb96f842026b4a76dd9
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:24:08 -0230

    GetAllUpdate

--- src/main/java/com/example/golfclub/service/MemberService.java
+++ src/main/java/com/example/golfclub/service/MemberService.java
@@ -5,6 +5,8 @@ import com.example.golfclub.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberService {
 
@@ -18,5 +20,10 @@ public class MemberService {
     //Save member
     public Member saveMember(Member member) {
         return memberRepository.save(member);
+    }
+
+    // Get all members
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
     }
 }
