7598cf5a6a3e7e7e3b643c797833d3757eb32866
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:21:00 -0230

    AddMem

--- src/main/java/com/example/golfclub/controller/MemberController.java
+++ src/main/java/com/example/golfclub/controller/MemberController.java
@@ -7,5 +7,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // End for adding new member
+    @PostMapping
+    public Member addMember(@RequestBody Member member) {
+        return memberService.saveMember(member);
+    }
 }
