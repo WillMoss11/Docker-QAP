cb2db617bc0882e24667a67d55bc0100a4468bbc
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:25:46 -0230

    imports

--- src/main/java/com/example/golfclub/GolfClubApplication.java
+++ src/main/java/com/example/golfclub/GolfClubApplication.java
@@ -1,4 +1,7 @@
 package com.example.golfclub;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class GolfClubApplication {
 }
