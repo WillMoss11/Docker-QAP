c8f11b32dff1ab77ad007b6fe707978eb718840a
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:12:04 -0230

    Imports

--- src/main/java/com/example/golfclub/repository/MemberRepository.java
+++ src/main/java/com/example/golfclub/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.example.golfclub.repository;
 
+import com.example.golfclub.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface MemberRepository {
 }
