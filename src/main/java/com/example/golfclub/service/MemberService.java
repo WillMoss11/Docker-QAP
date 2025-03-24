c76fa4728361274d3ab8b503024374b9055bc1fe
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:15:11 -0230

    MemberRepo import

--- src/main/java/com/example/golfclub/service/MemberService.java
+++ src/main/java/com/example/golfclub/service/MemberService.java
@@ -5,5 +5,13 @@ import com.example.golfclub.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 }
