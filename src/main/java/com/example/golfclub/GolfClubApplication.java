f9082b1011527dc3df6826cf97db8efdda84acdd
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:26:29 -0230

    Main run

--- src/main/java/com/example/golfclub/GolfClubApplication.java
+++ src/main/java/com/example/golfclub/GolfClubApplication.java
@@ -3,5 +3,10 @@ package com.example.golfclub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class GolfClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolfClubApplication.class, args);
+    }
 }
