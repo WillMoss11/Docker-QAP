4bc746daa23f305f49860cb4e3193c55b4b999d9
WillMoss11 <william.moss@keyin.com>
2025-03-24 03:19:15 -0230

    Imports added

--- src/main/java/com/example/golfclub/repository/TournamentRepository.java
+++ src/main/java/com/example/golfclub/repository/TournamentRepository.java
@@ -1,4 +1,7 @@
 package com.example.golfclub.repository;
 
+import com.example.golfclub.model.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface TournamentRepository {
 }
