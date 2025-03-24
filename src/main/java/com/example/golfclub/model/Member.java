095e3e82d55bc6f983aab5ff7208c08b5929c1fb
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:49:18 -0230

    Updates

--- src/main/java/com/example/golfclub/model/Member.java
+++ src/main/java/com/example/golfclub/model/Member.java
@@ -1,9 +1,10 @@
 package com.example.golfclub.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 public class Member {
