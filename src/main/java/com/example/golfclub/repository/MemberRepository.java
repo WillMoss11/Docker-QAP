db41c3edcd6cd926e5f50897b6e1f6c88a969c35
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:12:41 -0230

    Extension added

--- src/main/java/com/example/golfclub/repository/MemberRepository.java
+++ src/main/java/com/example/golfclub/repository/MemberRepository.java
@@ -3,5 +3,6 @@ package com.example.golfclub.repository;
 import com.example.golfclub.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
