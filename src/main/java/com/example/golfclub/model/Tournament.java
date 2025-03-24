f79ad89004e5c009f1f566bc9148a70253d909b3
WillMoss11 <william.moss@keyin.com>
2025-03-24 03:17:44 -0230

    Gets

--- src/main/java/com/example/golfclub/model/Tournament.java
+++ src/main/java/com/example/golfclub/model/Tournament.java
@@ -24,4 +24,60 @@ public class Tournament {
     )
     private List<Member> participatingMembers;
 
+    // Gets n Sets obv
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(double entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public double getCashPrizeAmount() {
+        return cashPrizeAmount;
+    }
+
+    public void setCashPrizeAmount(double cashPrizeAmount) {
+        this.cashPrizeAmount = cashPrizeAmount;
+    }
+
+    public List<Member> getParticipatingMembers() {
+        return participatingMembers;
+    }
+
+    public void setParticipatingMembers(List<Member> participatingMembers) {
+        this.participatingMembers = participatingMembers;
+    }
 }
