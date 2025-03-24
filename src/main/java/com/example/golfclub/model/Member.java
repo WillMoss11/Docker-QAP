cd4c886b8af14a308ab83040228ca54a9cfda5b2
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:07:45 -0230

    Gets n Sets

--- src/main/java/com/example/golfclub/model/Member.java
+++ src/main/java/com/example/golfclub/model/Member.java
@@ -41,5 +41,53 @@ public class Member {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getMembershipDuration() {
+        return membershipDuration;
+    }
+
+    public void setMembershipDuration(int membershipDuration) {
+        this.membershipDuration = membershipDuration;
+    }
+
 
 }
