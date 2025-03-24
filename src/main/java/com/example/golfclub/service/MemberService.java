7e699ab554694f384eb17a4e455f417ac5ff0672
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:14:04 -0230

    Imports

--- src/main/java/com/example/golfclub/service/MemberService.java
+++ src/main/java/com/example/golfclub/service/MemberService.java
@@ -1,4 +1,9 @@
 package com.example.golfclub.service;
 
+import com.example.golfclub.model.Member;
+import com.example.golfclub.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 public class MemberService {
 }
