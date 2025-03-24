dd1484f49fd5f665517943ef1c003b69da6d4ff5
WillMoss11 <william.moss@keyin.com>
2025-03-24 03:48:56 -0230

    Update

--- src/main/java/com/example/golfclub/repository/TournamentRepository.java
+++ src/main/java/com/example/golfclub/repository/TournamentRepository.java
@@ -2,7 +2,8 @@ package com.example.golfclub.repository;
 
 import com.example.golfclub.model.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
-
+    List<Tournament> findByLocation(String location);
 }
