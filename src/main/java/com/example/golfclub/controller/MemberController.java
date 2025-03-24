17e18ed6e96d242693e1639d350584c4741ba4ea
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:18:06 -0230

    Imports added

--- src/main/java/com/example/golfclub/controller/MemberController.java
+++ src/main/java/com/example/golfclub/controller/MemberController.java
@@ -1,4 +1,11 @@
 package com.example.golfclub.controller;
 
+import com.example.golfclub.model.Member;
+import com.example.golfclub.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class MemberController {
 }
