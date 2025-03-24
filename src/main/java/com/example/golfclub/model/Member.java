dbee67525b66a7fcf989cf4bb9e841d396e86840
WillMoss11 <william.moss@keyin.com>
2025-03-24 02:03:34 -0230

    Start

--- src/main/java/com/example/golfclub/model/Member.java
+++ src/main/java/com/example/golfclub/model/Member.java
@@ -12,5 +12,18 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String startDate;
+    private int membershipDuration; // Months for ex
+
+    //Default start (JPA)
+    public Member() {}
+
+    // Parameters
+
+
 
 }
