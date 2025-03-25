378bd0584a02314689af4b668f0e54f28dd4a85a
WillMoss11 <william.moss@keyin.com>
2025-03-24 21:42:02 -0230

    Adjusted getMember

--- src/main/java/com/example/golfclub/controller/MemberController.java
+++ src/main/java/com/example/golfclub/controller/MemberController.java
@@ -2,42 +2,41 @@ package com.example.golfclub.controller;
 
 import com.example.golfclub.model.Member;
 import com.example.golfclub.service.MemberService;
+import com.example.golfclub.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.example.golfclub.repository.MemberRepository;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    private MemberRepository memberRepository;
+    public MemberController(MemberService memberService, MemberRepository memberRepository) {
+        this.memberService = memberService;
+        this.memberRepository = memberRepository;
+    }
 
     // Search members by name
     @GetMapping("/search")
-    public List<Member> searchMembersByName(@RequestParam String name) {
+    public List<Member> getMemberByName(@RequestParam String name) {
         return memberRepository.findByNameContaining(name);
     }
 
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
-    // End for adding new member
+    // Add a new member
     @PostMapping
     public Member addMember(@RequestBody Member member) {
         return memberService.saveMember(member);
     }
 
-    // End for getting all members
+    // Get all members
     @GetMapping
     public List<Member> getAllMembers() {
         return memberService.getAllMembers();
     }
 }
+
--- src/main/java/com/example/golfclub/repository/MemberRepository.java
+++ src/main/java/com/example/golfclub/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package com.example.golfclub.repository;
 
 import com.example.golfclub.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByNameContaining(String name);
+    List<Member> findByNameContaining(String name);  // Make sure this is present
 }
+
